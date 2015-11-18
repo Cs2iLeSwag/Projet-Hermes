d0ed9c54c012f457a6aafa39641ab363c1f6923f
Cs2iLeSwag <j.leny@orange.fr>
2015-11-18 15:25:11 +0100

    Interface Update

--- Accueil/Accueil.Designer.cs
+++ Accueil/Accueil.Designer.cs
@@ -28,45 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accueil));
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.lblName = new System.Windows.Forms.Label();
             this.btnDiscussion = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSong = new System.Windows.Forms.Button();
             this.btnHG = new System.Windows.Forms.Button();
             this.btnAbsence = new System.Windows.Forms.Button();
             this.btnConnexion = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
-            this.toolStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imgAvatar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgAvatar)).BeginInit();
             this.SuspendLayout();
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripDropDownButton1,
-            this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // toolStripButton1
-            // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Segoe Print", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(161, 51);
+            this.lblName.Location = new System.Drawing.Point(162, 30);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(272, 56);
             this.lblName.TabIndex = 5;
@@ -83,24 +59,24 @@
             this.btnDiscussion.UseVisualStyleBackColor = true;
             this.btnDiscussion.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button1
+            // btnSong
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImage = global::Accueil.Properties.Resources.songOff;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(12, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(48, 51);
-            this.button1.TabIndex = 9;
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnSong.BackColor = System.Drawing.Color.Transparent;
+            this.btnSong.BackgroundImage = global::Accueil.Properties.Resources.songOff;
+            this.btnSong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSong.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSong.Location = new System.Drawing.Point(13, 19);
+            this.btnSong.Name = "btnSong";
+            this.btnSong.Size = new System.Drawing.Size(48, 51);
+            this.btnSong.TabIndex = 9;
+            this.btnSong.UseVisualStyleBackColor = false;
             // 
             // btnHG
             // 
             this.btnHG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnHG.ForeColor = System.Drawing.Color.Transparent;
             this.btnHG.Image = global::Accueil.Properties.Resources.connectOff;
-            this.btnHG.Location = new System.Drawing.Point(322, 110);
+            this.btnHG.Location = new System.Drawing.Point(323, 89);
             this.btnHG.Name = "btnHG";
             this.btnHG.Size = new System.Drawing.Size(51, 50);
             this.btnHG.TabIndex = 8;
@@ -111,7 +87,7 @@
             this.btnAbsence.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAbsence.ForeColor = System.Drawing.Color.Transparent;
             this.btnAbsence.Image = global::Accueil.Properties.Resources.connectPause;
-            this.btnAbsence.Location = new System.Drawing.Point(247, 110);
+            this.btnAbsence.Location = new System.Drawing.Point(248, 89);
             this.btnAbsence.Name = "btnAbsence";
             this.btnAbsence.Size = new System.Drawing.Size(51, 50);
             this.btnAbsence.TabIndex = 7;
@@ -122,29 +98,20 @@
             this.btnConnexion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConnexion.ForeColor = System.Drawing.Color.Transparent;
             this.btnConnexion.Image = global::Accueil.Properties.Resources.connect;
-            this.btnConnexion.Location = new System.Drawing.Point(171, 110);
+            this.btnConnexion.Location = new System.Drawing.Point(172, 89);
             this.btnConnexion.Name = "btnConnexion";
             this.btnConnexion.Size = new System.Drawing.Size(51, 50);
             this.btnConnexion.TabIndex = 2;
             this.btnConnexion.UseVisualStyleBackColor = true;
             this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
             // 
-            // pictureBox1
+            // imgAvatar
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(36, 59);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(107, 101);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // toolStripDropDownButton1
-            // 
-            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
-            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
-            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            this.imgAvatar.Location = new System.Drawing.Point(37, 38);
+            this.imgAvatar.Name = "imgAvatar";
+            this.imgAvatar.Size = new System.Drawing.Size(107, 101);
+            this.imgAvatar.TabIndex = 1;
+            this.imgAvatar.TabStop = false;
             // 
             // Accueil
             // 
@@ -152,20 +119,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(784, 361);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSong);
             this.Controls.Add(this.btnHG);
             this.Controls.Add(this.btnAbsence);
             this.Controls.Add(this.btnDiscussion);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.btnConnexion);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.imgAvatar);
             this.Name = "Accueil";
             this.Text = "Accueil";
             this.Load += new System.EventHandler(this.Accueil_Load);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgAvatar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,15 +137,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgAvatar;
         private System.Windows.Forms.Button btnConnexion;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Button btnDiscussion;
         private System.Windows.Forms.Button btnAbsence;
         private System.Windows.Forms.Button btnHG;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSong;
     }
 }
