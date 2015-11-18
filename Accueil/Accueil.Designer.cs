3d539bfa530d17fe7661bdef60d9859b9d5cba5d
Pondaven <epondaven@cs2i-lorient.fr>
2015-11-18 11:04:20 +0100

    Ajout btn

--- Accueil/Accueil.Designer.cs
+++ Accueil/Accueil.Designer.cs
@@ -32,6 +32,7 @@
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,12 +64,22 @@
             this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
             this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Accueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Accueil";
             this.Text = "Accueil";
@@ -84,5 +95,6 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.Button button1;
     }
 }
