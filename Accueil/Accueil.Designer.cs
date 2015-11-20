86089e3ed4044d698b9203c399ffcc713d0e67b6
Cs2iLeSwag <j.leny@orange.fr>
2015-11-20 10:45:50 +0100

    Bug

--- Accueil/Accueil.Designer.cs
+++ Accueil/Accueil.Designer.cs
@@ -115,16 +115,7 @@
             this.imgAvatar.Size = new System.Drawing.Size(110, 103);
             this.imgAvatar.TabIndex = 10;
             this.imgAvatar.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(24, 209);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(283, 129);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+
             // 
             // Accueil
             // 
